72605517503d2816a381d9c9d3ec0db3951dea27
Varunan_V <47948397+varunan-vara@users.noreply.github.com>
2021-03-01 13:29:02 -0500

    Website works as intended, adding a few changes

--- js/runPython.js
+++ js/runPython.js
@@ -31,8 +31,51 @@ pypyjs.ready().then( function () {
     }, 1000)
 });
 
-var pythonRun = new pypyjs({
-    stdout: function (data) {
-        
-    },
+//Define some variables
+var outputwrap = document.getElementById("terminalbox");
+function outputLine (data) {
+    var outputy = document.createElement("p");
+    outputy.className = "outputText";
+    outputy.innerHTML = String(data);
+
+    //Add new line to terminal
+    outputwrap.appendChild(outputy);
+    var br = document.createElement("br");
+    outputwrap.appendChild(br);
+}
+
+function outputErr (data) {
+    document.getElementById("runButton").innerHTML = "Error";
+    var outputy = document.createElement("p");
+    outputy.className = "errText";
+    outputy.innerHTML = String(data);
+
+    //Add new line to terminal
+    outputwrap.appendChild(outputy);
+    var br = document.createElement("br");
+    outputwrap.appendChild(br);
+    setTimeout(function(){document.getElementById("runButton").innerHTML = "Run Code"}, 2000)
+}
+
+function runCode() {
+    document.getElementById("runButton").innerHTML = "Running...";
+    setTimeout(function(){document.getElementById("runButton").innerHTML = "Run Code"}, 2000)
+}
+
+document.getElementById("runButton").addEventListener("click", function (){
+    var pythonRun = new pypyjs({
+        stdout: function (data) {
+            outputLine(data);
+        },
+        stdin: function () {
+            runCode();
+        },
+        stderr: function (data) {
+            outputErr(data);
+        },
+    });
+    pythonRun.ready().then(function(){
+        var inputy = document.getElementById("IDE").value;
+        return pythonRun.exec(inputy);
+    });
 });
