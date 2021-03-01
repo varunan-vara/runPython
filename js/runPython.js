f4df29c6a8a6e7fe2d35ced0c569212c9d8fa64b
Varunan_V <47948397+varunan-vara@users.noreply.github.com>
2021-03-01 07:38:25 -0500

    Experimenting with jqconsole

--- js/runPython.js
+++ js/runPython.js
@@ -5,7 +5,7 @@ pypyjs.ready().then( function () {
     setTimeout(function() {
         document.getElementById("infoplacer").style.color = "#F8F9FA";
         document.getElementById("infoplacer").innerHTML = "Pypyjs is loaded, and ready for use.";
-        document.getElementById("IDE").disabled = false;
+        //document.getElementById("IDE").disabled = false;
         function addToString (stringy) {
             stringplacer += stringy
         }
@@ -31,12 +31,26 @@ pypyjs.ready().then( function () {
     }, 1000)
 });
 
-/*
-.runButton:hover {
-    cursor: no-drop;
+//Initiate jqconsole in #inputbox
+var term = $("#inputbox").jqconsole("Launched jq-console", ">>>", "...");
+term.SetIndentWidth(4);
+
+//RegisterMatching Pairs Below:
+term.RegisterMatching("{", "}", "brackets");
+term.RegisterMatching("(", ")", "parantheses");
+term.RegisterMatching("[", "[", "square-brackets");
+term.RegisterMatching("'", "'", "string");
+term.RegisterMatching('"', '"', "string");
+
+pypyjs.stdout = pypyjs.stderr = function(data) {
+    term.Write(data, "output");
 }
 
-^^Implement, with runbutton turning to pointer when loaded
-*/
-
-
+pypyjs.repl(function(ps1) {
+    term.SetPromptLabel(ps1);
+    return new Promise(function(resolve, reject){
+        term.Prompt(true, function (input) {
+            resolve(input);
+        });
+    });
+});
